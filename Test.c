e3175c6875348e1c19a4b40edc7a3d216cc5c5b6
nshuddar <snithyananda030@gmail.com>
2023-05-04 16:14:54 +0530

    first commit

---
+++ Test.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+int addition(int a, int b);
+{
+  int c = 0;
+  
+ c = a+ b;
+ 
+ return c;
+ 
+}
+
+int subtraction(int a, int b);
+{
+  int c = 0;
+  
+ c = a- b;
+ 
+ return c;
+  
+}
+
+
+int multiplication(int a, int b);
+{
+  int c = 0;
+  
+ c = a * b;
+ 
+ return c;
+  
+}
